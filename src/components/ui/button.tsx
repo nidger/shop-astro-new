dac74198a98d6758e9debb4e0dcf8c916c99e59c
nidger <nprior70@yahoo.com>
2025-08-19 11:56:42 +0100

    made spinner on button bigger

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -6,7 +6,7 @@ import { Loader2 } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -87,12 +87,12 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       >
         {loading ? (
-          <Loader2 className="animate-spin" />
+          <Loader2 className="animate-spin size-6" />
         ) : (
           <>
-            {icon && iconPosition === "left" && icon}
+            {icon && iconPosition === "left" && <span className="size-4">{icon}</span>}
             {children}
-            {icon && iconPosition === "right" && icon}
+            {icon && iconPosition === "right" && <span className="size-4">{icon}</span>}
           </>
         )}
       </Comp>
