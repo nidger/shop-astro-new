c1f670fb89e86ffe24b82b1c83bba5c0c4c8c57b
nidger <nprior70@yahoo.com>
2025-08-24 07:29:42 +0100

    checked ability to add images for carousel

--- src/data/products.ts
+++ src/data/products.ts
@@ -19,7 +19,7 @@ export interface Product {
 
 export const allProducts: Product[] = [
   { id: "1", slug: "embroidered-astro-bucket-hat", collectionId: "summer", title: "Embroidered Astro Bucket Hat", price: "$30.00", colors: ["#F5F5DC", "#FFB6C1", "#F0E68C"], images: [{src: bucketHat, alt: "Embroidered Astro Bucket Hat"}, {src: beachTowel, alt: "Embroidered Astro Bucket Hat"}, {src: tiedyeShirt, alt: "Embroidered Astro Bucket Hat"}] },
-  { id: "2", slug: "astro-striped-beach-towel", collectionId: "summer", title: "Astro Striped Beach Towel", price: "$35.00", images: [{src: beachTowel, alt: "Astro Striped Beach Towel"}] },
+  { id: "2", slug: "astro-striped-beach-towel", collectionId: "summer", title: "Astro Striped Beach Towel", price: "$35.00", images: [{src: beachTowel, alt: "Astro Striped Beach Towel"}, {src: bucketHat, alt: "Astro Striped Beach Towel"}] },
   { id: "3", slug: "oversized-interplanetary-astro-t-shirt", collectionId: "summer", title: "Oversized Interplanetary Astro T-Shirt", price: "$34.00", images: [{src: tiedyeShirt, alt: "Oversized Interplanetary Astro T-Shirt"}] },
   { id: "4", slug: "oversized-astral-patches-tie-dye-t-shirt", collectionId: "summer", title: "Oversized Astral Patches Tie-Dye T-Shirt", price: "$38.00", images: [{src: tiedyeShirt, alt: "Oversized Astral Patches Tie-Dye T-Shirt"}] },
   { id: "5", slug: "astronaut-blue-zip-up-hoodie", collectionId: "apparel", title: "Astronaut Blue Zip-up Hoodie", price: "$45.00", images: [{src: blueHoodie, alt: "Astronaut Blue Zip-up Hoodie"}] },
